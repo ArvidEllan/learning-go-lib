e8ba77ce16fc5fd1378a4ce5b2da85cd46094095
ArvidEllan <ellanwambugu6@gmail.com>
2023-02-08 22:39:46 +0300

    updated api on main

--- main.go
+++ main.go
@@ -1,6 +1,13 @@
 package main
 
-func main() {
+import "github.com/gin-gonic/gin"
 
-	fmt.println("hello world")
+func main() {
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Hello, welcome to the learning-go-api",
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
