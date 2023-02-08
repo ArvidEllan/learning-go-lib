c1e13509493d6be9430562ac27747d47862d3133
ArvidEllan <ellanwambugu6@gmail.com>
2023-02-08 22:40:49 +0300

    programming go file for testing

---
+++ programming.go
@@ -0,0 +1,40 @@
+package programming
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/renato0307/learning-go-lib/programming"
+)
+
+// postUuidOutput is the output of the "POST /programming/uuid" action
+type postUuidOutput struct {
+	UUID string `json:"uuid"`
+}
+
+// SetRouterGroup defines all the routes for the programming functions
+func SetRouterGroup(base *gin.RouterGroup) *gin.RouterGroup {
+	programmingGroup := base.Group("/programming")
+	{
+		programmingGroup.POST("/uuid", postUuid())
+		// Add here more functions in the programming category
+	}
+
+	return programmingGroup
+}
+
+// postUuid handles the uuid request.
+// It returns 200 on success.
+// Reads the "no-hyphens" parameter from the query string to support
+// UUIDs without hyphens.
+func postUuid() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		noHyphensParamValue := c.Query("no-hyphens")
+		withoutHyphens := noHyphensParamValue == "true"
+
+		uuid := programming.NewUuid(withoutHyphens)
+		output := postUuidOutput{UUID: uuid}
+
+		c.JSON(http.StatusOK, output)
+	}
+}
