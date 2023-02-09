c69f6b0a86cc5c2cbfbef943844e7d8028df3cb9
ArvidEllan <ellanwambugu6@gmail.com>
2023-02-09 12:34:03 +0300

    programming test functionality

--- programming_test.go
+++ programming_test.go
@@ -40,3 +40,23 @@ func TestPostUuid(t *testing.T) {
 	assert.Len(t, output.UUID, 36)
 	assert.Contains(t, output.UUID, "-")
 }
+
+func TestPostUuidWithNoHyphen(t *testing.T) {
+	// arrange
+	r := setupGin()
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("POST", "/v1/programming/uuid?no-hyphens=true", nil)
+
+	// act
+	r.ServeHTTP(w, req)
+
+	// assert
+	assert.Equal(t, w.Code, http.StatusOK)
+
+	output := postUuidOutput{}
+	err := json.Unmarshal(w.Body.Bytes(), &output)
+
+	assert.Nil(t, err)
+	assert.Len(t, output.UUID, 32)
+	assert.NotContains(t, output.UUID, "-")
+}
